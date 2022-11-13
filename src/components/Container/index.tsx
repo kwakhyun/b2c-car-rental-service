2c75017a089aa53e16599e0bab6abb24e83087f0
kwakhyun <khyun2348@naver.com>
2022-11-14 02:09:01 +0900

    Feat: Container component

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,27 @@
+import styled, { FlattenInterpolation, ThemeProps } from 'styled-components';
+import { positionCenter, responsive } from '../../styles/mixin';
+
+type ContainerProps = {
+  children: React.ReactNode;
+  customStyle?: FlattenInterpolation<ThemeProps<unknown>>;
+};
+
+export default function Container({ children, customStyle }: ContainerProps) {
+  return <StyledContainer customStyle={customStyle}>{children}</StyledContainer>;
+}
+
+const StyledContainer = styled.div<{ customStyle: FlattenInterpolation<ThemeProps<unknown>> | undefined }>`
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  ${responsive('desktop')} {
+    ${positionCenter()};
+    width: 390px;
+    height: 750px;
+    box-shadow: 0 0 8px ${({ theme }) => theme.gray};
+  }
+
+  ${({ customStyle }) => customStyle}
+`;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import CarItem from '../../components/CarItem';
 import Category from '../../components/Category';
+import Container from '../../components/Container';
 import Header from '../../components/Header';
 import { useGetVehicles } from '../../hooks/useGetVehicles';
 import { flexBox } from '../../styles/mixin';
@@ -19,17 +20,23 @@ export default function Home() {
   }, [segment]);
 
   return (
-    <>
+    <Container customStyle={StyledContainer}>
       <Header title="전체차량" />
       <Category segment={segment} setSearchParams={setSearchParams} />
+
       {isLoading && <StyledNotice>불러오는 중</StyledNotice>}
 
       {!isLoading && vehicles?.map((vehicle) => <CarItem key={vehicle.id} car={vehicle} />)}
 
       {!isLoading && !vehicles?.length && <StyledNotice>차량이 없습니다.</StyledNotice>}
-    </>
+    </Container>
   );
 }
+
+const StyledContainer = css`
+  height: 100vh;
+  padding-bottom: ${({ theme }) => theme.paddingVertical};
+`;
 
 const StyledNotice = styled.div`
   ${flexBox()};
